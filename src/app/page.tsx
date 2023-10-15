99f4ac940e3ff16fc78da3176576683cc629d5f1
MusaddekAli <musaddekali.dev@gmail.com>
2023-10-15 10:03:06 +0600

    working on sidebar dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,130 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="dashboard min-h-screen overflow-hidden flex relative">
+      {/* === Sidebar === */}
+      <aside className="sidebar h-screen overflow-hidden w-sidebarWidth shrink-0 bg-primary-dark">
+        <div className="custom_scrollbar h-full overflow-y-auto">
+          <div className="p-3 bg-inherit">
+            {/* ==== Logo ==== */}
+            <div className="logo_area flex items-center justify-center w-auto h-14 overflow-hidden">
+              <Image
+                width={200}
+                height={80}
+                className="w-full h-full object-contain"
+                src="/assets/images/demo-logo.png"
+                alt="site_logo"
+              />
+            </div>
+          </div>
+          {/* === Links ==== */}
+          <nav className="mt-6">
+            <ul className="flex flex-col">
+              <li>
+                <h3
+                  aria-label="Dashboard links category"
+                  className="px-6 pt-6 pb-2 text-white text-opacity-90 text-xs font-medium"
+                >
+                  Basic
+                </h3>
+                <span className="dashboard_sidebar_link dashboard_sidebar_link_active">
+                  Dashboard
+                </span>
+              </li>
+              <li>
+                <h3
+                  aria-label="Dashboard links category"
+                  className="px-6 pt-6 pb-2 text-white text-opacity-90 text-xs font-medium"
+                >
+                  User
+                </h3>
+                <span className="dashboard_sidebar_link">Profile</span>
+              </li>
+              <li>
+                <div className="dashboard_sidebar_link">Settings</div>
+                <div className="dropdowon_content">
+                  <div className="bg-primary px-10 py-4">
+                    <ul>
+                      <li>Dropdonw item 1</li>
+                      <li>Dropdonw item 2</li>
+                      <li>Dropdonw item 3</li>
+                      <li>Dropdonw item 4</li>
+                      <li>Dropdonw item 5</li>
+                      <li>Dropdonw item 6</li>
+                      <li>Dropdonw item 7</li>
+                      <li>Dropdonw item 8</li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <h3
+                  aria-label="Dashboard links category"
+                  className="px-6 pt-6 pb-2 text-white text-opacity-90 text-xs font-medium"
+                >
+                  Shop
+                </h3>
+                <span className="dashboard_sidebar_link">Invoice</span>
+              </li>
+              <li>
+                <span className="dashboard_sidebar_link">Products</span>
+              </li>
+              <li>
+                <span className="dashboard_sidebar_link">Order</span>
+              </li>
+              <li>
+                <span className="dashboard_sidebar_link">Reports</span>
+              </li>
+              <li>
+                <span className="dashboard_sidebar_link">Agents</span>
+              </li>
+              <li>
+                <span className="dashboard_sidebar_link">Customers</span>
+              </li>
+              <li>
+                <span className="dashboard_sidebar_link">Settings</span>
+              </li>
+              <li>
+                <span className="dashboard_sidebar_link">Logout</span>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </aside>{" "}
+      {/* End Sidebar  */}
+      <main className="h-full flex flex-col bg-red-100 grow">
+        <header className="bg-slate-300">
+          <nav className="flex items-center justify-between p-4">
+            <input
+              className="px-5 py-2 leading-none rounded-md"
+              type="text"
+              placeholder="Search..."
+            />
+            <ul className="flex items-center gap-5">
+              <li>Noti</li>
+              <li>Message</li>
+              <li>
+                <div>
+                  <span className="flex items-center justify-center w-12 h-12 bg-red-400 rounded-full overflow-hidden border border-blue-500">
+                    MA
+                  </span>
+                </div>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <h2>Main content</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi
+          praesentium dicta commodi iste distinctio laudantium? Dolore eos
+          voluptatibus maiores vitae id eligendi fugit consequuntur similique
+          eaque, placeat qui quae molestiae, optio ratione officiis in autem.
+          Omnis minus ad mollitia id, sapiente itaque ab consequatur, iusto
+          voluptatibus incidunt natus, voluptates quasi.
+        </p>
+      </main>
+    </div>
+  );
 }
